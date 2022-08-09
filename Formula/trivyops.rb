9d5df432b176bab27122f15d0cc80f19d537a653
goreleaserbot <goreleaser@steffen-rumpf.de>
2022-08-09 16:04:20 +0200

    Brew formula update for trivyops version v0.10-dev

--- Formula/trivyops.rb
+++ Formula/trivyops.rb
@@ -9,17 +9,17 @@ class Trivyops < Formula
   license "Apache-2.0"
 
   on_macos do
-    if Hardware::CPU.arm?
-      url "https://github.com/steffakasid/trivyops/releases/download/v0.10-dev/trivyops_0.10-dev_Darwin_arm64.tar.gz"
-      sha256 "d0ae7113c8df67d56a76f0b9d9136ab6df83c64f13a9f9586f7a3837e31366e9"
+    if Hardware::CPU.intel?
+      url "https://github.com/steffakasid/trivyops/releases/download/v0.10-dev/trivyops_0.10-dev_Darwin_x86_64.tar.gz"
+      sha256 "b865e330a06d940d10de0d176163698698de23b98751f2329ec0172ccb3ebe3b"
 
       def install
         bin.install "trivyops"
       end
     end
-    if Hardware::CPU.intel?
-      url "https://github.com/steffakasid/trivyops/releases/download/v0.10-dev/trivyops_0.10-dev_Darwin_x86_64.tar.gz"
-      sha256 "b81d5bdd34a5291faef3ca7b1a0ff66f94b9bbbbdd724ca17e75c17cba78e01a"
+    if Hardware::CPU.arm?
+      url "https://github.com/steffakasid/trivyops/releases/download/v0.10-dev/trivyops_0.10-dev_Darwin_arm64.tar.gz"
+      sha256 "d9a5fd6496f1f441f954147f52c4abd40a145436b536605604f6613d43a48468"
 
       def install
         bin.install "trivyops"
@@ -30,7 +30,7 @@ class Trivyops < Formula
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
       url "https://github.com/steffakasid/trivyops/releases/download/v0.10-dev/trivyops_0.10-dev_Linux_arm64.tar.gz"
-      sha256 "66d2af7c18d8b8952aafc9df826956bcae4c1cf01c2c072ba2846d7c15ecb95f"
+      sha256 "d53fac8229430f1539e9ddc5c2a65b606b96aba1c82838aaf3d642346ad1accc"
 
       def install
         bin.install "trivyops"
@@ -38,7 +38,7 @@ class Trivyops < Formula
     end
     if Hardware::CPU.intel?
       url "https://github.com/steffakasid/trivyops/releases/download/v0.10-dev/trivyops_0.10-dev_Linux_x86_64.tar.gz"
-      sha256 "2791fac83f83078aaeebb49be80a526ad6fc10882c498d4006068147e4b52b5c"
+      sha256 "b8a95d6d5b4c2715b8efc45ea3b784ce3622665ed85ca9af86adbb4ec0eb8358"
 
       def install
         bin.install "trivyops"
